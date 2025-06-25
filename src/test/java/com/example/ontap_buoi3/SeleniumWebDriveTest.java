86b481843d23f65577f7d3204dba93124a41676f
phuongnh15 <162479809+phuongnh15@users.noreply.github.com>
2025-06-25 11:33:41 +0700

    update selenium 11h33 ngay 25/6

--- src/test/java/com/example/ontap_buoi3/SeleniumWebDriveTest.java
+++ src/test/java/com/example/ontap_buoi3/SeleniumWebDriveTest.java
@@ -10,38 +10,38 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class SeleniumWebDriveTest {
-    private WebDriver webDriver;
-
-    @BeforeEach
-    void setUp(){
-        WebDriverManager.chromedriver().setup();
-        webDriver = new ChromeDriver();
-    }
-
-    @Test
-    public void dangNhap() throws InterruptedException {
-        //truy cap trang
-        webDriver.get("https://anupdamoda.github.io/AceOnlineShoePortal/index.html");
-
-        //click vao menu
-        webDriver.findElement(By.xpath("//*[@id=\"menuToggle\"]/input")).click();
-
-        Thread.sleep(2000);
-
-        //click vao dang nhap
-        webDriver.findElement(By.xpath("//*[@id=\"menu\"]/a[2]/li")).click();
-
-        //nhap user pass
-        webDriver.findElement(By.xpath("//*[@id=\"usr\"]")).sendKeys("sa");
-        webDriver.findElement(By.xpath("//*[@id=\"pwd\"]")).sendKeys("sa");
-
-        //click login
-        webDriver.findElement(By.xpath("//*[@id=\"second_form\"]/input")).click();
-
-        WebElement webElement = webDriver.findElement(By.xpath("//*[@id=\"ShoeType\"]"));
-        Assertions.assertEquals("Formal Shoes",webElement.getText());
-
-        //thoat
-        webDriver.quit();
-    }
+//    private WebDriver webDriver;
+//
+//    @BeforeEach
+//    void setUp(){
+//        WebDriverManager.chromedriver().setup();
+//        webDriver = new ChromeDriver();
+//    }
+//
+//    @Test
+//    public void dangNhap() throws InterruptedException {
+//        //truy cap trang
+//        webDriver.get("https://anupdamoda.github.io/AceOnlineShoePortal/index.html");
+//
+//        //click vao menu
+//        webDriver.findElement(By.xpath("//*[@id=\"menuToggle\"]/input")).click();
+//
+//        Thread.sleep(2000);
+//
+//        //click vao dang nhap
+//        webDriver.findElement(By.xpath("//*[@id=\"menu\"]/a[2]/li")).click();
+//
+//        //nhap user pass
+//        webDriver.findElement(By.xpath("//*[@id=\"usr\"]")).sendKeys("sa");
+//        webDriver.findElement(By.xpath("//*[@id=\"pwd\"]")).sendKeys("sa");
+//
+//        //click login
+//        webDriver.findElement(By.xpath("//*[@id=\"second_form\"]/input")).click();
+//
+//        WebElement webElement = webDriver.findElement(By.xpath("//*[@id=\"ShoeType\"]"));
+//        Assertions.assertEquals("Formal Shoes",webElement.getText());
+//
+//        //thoat
+//        webDriver.quit();
+//    }
 }
